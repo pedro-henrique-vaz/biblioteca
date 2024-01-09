470a53f0f224b888eb2c2c99a5f4698d868dba54
Pedro Henrique <vazpedro06@gmail.com>
2024-01-09 17:51:01 -0300

    removendo .idea

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 * listar livros por titulo ou autor
 * emprestar livros ate uma data
 * cobrar 2 reais por dia caso a pessoa n de o livro na data
+
+
+cadastra livros
+listar todos os livros
+listar livros por titulo ou autor
+emprestar livros ate uma data
+cobrar 2 reais por dia caso a pessoa n de o livro na data
---
+++ projetinho.js
@@ -0,0 +1,20 @@
+class Livros {
+    constructor(titulo, autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataEmprestimo = null;
+    }
+
+    emprestar(dataDevolucao) {
+        this.dataEmprestimo = dataDevolucao;
+    }
+
+    calcularMulta(dataDevolucao) {
+        if (this.dataEmprestimo && dataDevolucao > this.dataEmprestimo) {   // ve se o livro foi emprestado e se a data da devolução ultrapassa a data do empréstimo.
+            const diasAtraso = Math.ceil((dataDevolucao - this.dataEmprestimo) / (1000 * 60 * 60 * 24)); // essa parte converte a diferença de milissegundos para dias.
+            const multa = diasAtraso * 2;
+            return multa;
+        }
+        return 0;
+    }
+}
