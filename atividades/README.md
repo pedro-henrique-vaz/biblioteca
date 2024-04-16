9a69e1c648ca537818fe240bf813f5020f50d92f
Pedro Henrique <vazpedro06@gmail.com>
2024-04-16 17:19:09 -0300

    atividades javascript estagio

---
+++ atividades/README.md
@@ -0,0 +1,3 @@
+retrnar a soma e nao o indice
+comecar do segundo elemento
+o primeiro ser sempre padrao
--- atividades/questao21.ts
+++ atividades/questao21.ts
@@ -1,16 +1,22 @@
 async function main() {
     let primeiroArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    let somandoValoresDoArray = addingTheValues(primeiroArr, (el0: number, el1: number): number => {
-        return el0 + el1
-    })
+    let somandoValoresDoArray: number = addingTheValues(primeiroArr,(el0: number, somaTotal: number): number => {
+        return el0 + somaTotal
+    }, 0)
     console.log(somandoValoresDoArray)
+
+    let multiplicandoValoresDoArray: number = addingTheValues(primeiroArr,(el0: number, somaTotal: number): number => {
+        return el0 * somaTotal
+    }, 1)
+    console.log(multiplicandoValoresDoArray)
 }
-function addingTheValues(primeiroArr: number[], extractFn: (el0:number, el1:number)=> number): number {
-    let arraySecundario: number = 0
-    for(let i: number = 0; i < primeiroArr.length; i++) {
-        if (extractFn(primeiroArr[i], i)) {
-           
-        }
+
+function addingTheValues(primeiroArr: number[], extractFn: (el0: number, somaTotal: number) => number, valorInicial:number): number {
+    let somaTotal: number = valorInicial
+    for (let i: number = 0; i < primeiroArr.length; i++) {
+        somaTotal = extractFn(primeiroArr[3], somaTotal)
     }
+    return somaTotal
 }
+
 main()
