adda0835fa239341a771afb3ca2a53cfa2cda892
Pedro Henrique <vazpedro06@gmail.com>
2024-04-23 18:43:15 -0300

    atividades javascript estagio

--- atividades/README.md
+++ atividades/README.md
@@ -1,3 +1,1 @@
-crie 5 livros que seja uma classe ou interface que tenha o nome e o autor OK
-criar outra classe ou interface que vai ter uma propriedade chamada livros que vai ser uma lista de livros OK
-adicionar os livros dentro dessa lista OK
+adicionar o livro que foi passado como parametro na biblioteca 
--- atividades/questao22.ts
+++ atividades/questao22.ts
@@ -5,37 +5,59 @@ interface Livro {
 interface Biblioteca {
     livros: Livro[]
 }
-async function main(){
-    const livro1: Livro = {
-        nome: 'Ilíada e Odisséia',
-        autor: 'Homero',
-    }
-    const livro2: Livro = {
-        nome: "Guerra e Paz",
-        autor: "Liev Tosltói"
-    }
-    const livro3: Livro = {
-        nome: "Orgulho e Preconceito",
-        autor: "Jane Austen"
-    }
-    const livro4: Livro = {
-        nome: "Cem Anos de Solidão",
-        autor: "Gabriel Garcia Marques"
-    }
-    const livro5: Livro = {
-        nome: "Dom Quixote",
-        autor: "Miguel de Cervantes"
-    }
-    const armazemDeLivro: Biblioteca = {
-        livros: [livro1, livro2, livro3, livro4, livro5]
-    }
-    let parametros = listagemDosLivros(armazemDeLivro)
-}
 
+async function main(){
+    let biblioteca = criarBiblioteca()
+    let livro1: Livro = criarLivro('Ilíada e Odisséisa', 'Homero')
+    let livro2: Livro = criarLivro('Guerra e Paz', 'Liev Tosltói')
+    let livro3: Livro = criarLivro('Orgulho e Preconceito', 'Jane Austen')
+    let livro4: Livro = criarLivro('Cem Anos de Solidão', 'Gabriel Garcia Marques')
+    let livro5: Livro = criarLivro('Dom Quixote', 'Miguel de Cervantes')
+
+    // biblioteca.livros = []
+    adicionarLivroNaBiblioteca(biblioteca, livro1)
+    adicionarLivroNaBiblioteca(biblioteca, livro1)
+    // biblioteca.livros = [livro1]
+
+    adicionarLivroNaBiblioteca(biblioteca, livro2)
+    adicionarLivroNaBiblioteca(biblioteca, livro2)
+    // biblioteca.livros = [livro1, livro2]
+
+    adicionarLivroNaBiblioteca(biblioteca, livro3)
+    // biblioteca.livros = [livro1, livro2, livro3]
+
+    adicionarLivroNaBiblioteca(biblioteca, livro4)
+    // biblioteca.livros = [livro1, livro2, livro3, livro4]
+
+    adicionarLivroNaBiblioteca(biblioteca, livro5)
+    // biblioteca.livros = [livro1, livro2, livro3, livro4, livro5]
+
+    listagemDosLivros(biblioteca)
+}
 function listagemDosLivros(checagem: Biblioteca) {
     console.log("Livros Disponiveis: ")
     checagem.livros.forEach((el: Livro) => {
-        console.log(`Nome: ${el.nome}    Autor: ${el.autor}`)
+        console.log(`Nome - ${el.nome};  Autor - ${el.autor}`)
     })
+}
+function criarLivro(nome: string, autor: string): Livro {
+    return {
+        nome: nome,
+        autor: autor,
+    }
+}
+function criarBiblioteca(): Biblioteca {
+    return {
+        livros: []
+    }
+}
+function adicionarLivroNaBiblioteca(biblioteca: Biblioteca, livro: Livro){
+    for(let i: number = 0; i < biblioteca.livros.length; i++) {
+        let l = biblioteca.livros[i]
+        if(l.autor === livro.autor && l.nome === livro.nome){
+            return
+        }
+    }
+    biblioteca.livros.push(livro)
 }
 main()
