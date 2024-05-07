0e781e484a1a805204db0793db39aedeaf16bec0
Pedro Henrique <vazpedro06@gmail.com>
2024-05-07 16:27:32 -0300

    atividades javascript estagio

--- atividades/questao22.ts
+++ atividades/questao22.ts
@@ -8,36 +8,49 @@ interface Biblioteca {
 }
 
 async function main(){
-    let biblioteca = criarBiblioteca()
+    let biblioteca1 = criarBiblioteca()
+    let biblioteca2 = criarBiblioteca()
     let livro1: Livro = criarLivro('Ilíada e Odisséisa', 'Homero')
     let livro2: Livro = criarLivro('Guerra e Paz', 'Liev Tosltói')
     let livro3: Livro = criarLivro('Orgulho e Preconceito', 'Jane Austen')
     let livro4: Livro = criarLivro('Cem Anos de Solidão', 'Gabriel Garcia Marques')
     let livro5: Livro = criarLivro('Dom Quixote', 'Miguel de Cervantes')
+    let livro6: Livro = criarLivro('Harry Potter', 'J.K')
+    let livro7: Livro = criarLivro('Game of Thrones', 'Martin')
+    let livro8: Livro = criarLivro('Lord of the Rings', 'Tolkien')
 
-    // biblioteca.livros = []
-    adicionarLivroNaBiblioteca(biblioteca, livro1)
-    adicionarLivroNaBiblioteca(biblioteca, livro1)
-    // biblioteca.livros = [livro1]
+    // biblioteca1.livros = []
+    adicionarLivroNaBiblioteca(biblioteca1, livro1)
+    adicionarLivroNaBiblioteca(biblioteca1, livro1)
+    // biblioteca1.livros = [livro1]
 
-    adicionarLivroNaBiblioteca(biblioteca, livro2)
-    adicionarLivroNaBiblioteca(biblioteca, livro2)
-    // biblioteca.livros = [livro1, livro2]
+    adicionarLivroNaBiblioteca(biblioteca1, livro2)
+    adicionarLivroNaBiblioteca(biblioteca1, livro2)
+    // biblioteca1.livros = [livro1, livro2]
 
-    adicionarLivroNaBiblioteca(biblioteca, livro3)
-    // biblioteca.livros = [livro1, livro2, livro3]
+    adicionarLivroNaBiblioteca(biblioteca1, livro3)
+    // biblioteca1.livros = [livro1, livro2, livro3]
 
-    adicionarLivroNaBiblioteca(biblioteca, livro4)
-    // biblioteca.livros = [livro1, livro2, livro3, livro4]
+    adicionarLivroNaBiblioteca(biblioteca1, livro4)
+    adicionarLivroNaBiblioteca(biblioteca2, livro4)
+    // biblioteca1.livros = [livro1, livro2, livro3, livro4]
 
-    adicionarLivroNaBiblioteca(biblioteca, livro5)
-    // biblioteca.livros = [livro1, livro2, livro3, livro4, livro5]
+    adicionarLivroNaBiblioteca(biblioteca1, livro5)
+    adicionarLivroNaBiblioteca(biblioteca2, livro5)
+    // biblioteca1.livros = [livro1, livro2, livro3, livro4, livro5]
 
-    listagemDosLivros(biblioteca)
-    let emprestimo: string = await questionStr('Voce quer pegar emprestado algum livro? ')
-    let livroEmprestado = emprestarLivro(biblioteca, emprestimo)
-    console.log(livroEmprestado)
-    listagemDosLivros(biblioteca)
+    adicionarLivroNaBiblioteca(biblioteca2, livro6)
+    adicionarLivroNaBiblioteca(biblioteca2, livro7)
+    adicionarLivroNaBiblioteca(biblioteca2, livro8)
+
+    // listagemDosLivros(biblioteca1)
+    let livroEmprestado1 = emprestarLivro(biblioteca1, 'Guerra e Paz')
+    let livroEmprestado2 = emprestarLivro(biblioteca1, 'Game of Thrones')
+    let livroEmprestado3 = emprestarLivro(biblioteca2, 'Harry Potter')
+    let livroEmprestado4 = emprestarLivro(biblioteca2, 'Orgulho e Preconceito')
+    // console.log(livroEmprestado1)
+    listagemDosLivros(biblioteca1)
+    listagemDosLivros(biblioteca2)
 }
 function listagemDosLivros(checagem: Biblioteca) {
     console.log("Livros Disponiveis: ")
