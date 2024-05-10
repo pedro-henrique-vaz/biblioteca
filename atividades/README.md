dfeffe3f3b711add71c7bdf57c2140993bd6a081
Pedro Henrique <vazpedro06@gmail.com>
2024-05-10 20:31:40 -0300

    atividades javascript estagio

---
+++ atividades/README.md
@@ -0,0 +1,4 @@
+retornar o valor da multa
+apos 7 dias = 1
+antes de 7 dias = -1
+em 7 dias = 0
--- atividades/questao22.ts
+++ atividades/questao22.ts
@@ -10,6 +10,10 @@ interface Emprestimo {
     livro: Livro
     dataContrato: Date
 }
+interface Recibo {
+    multa: number
+    dataDevolucao: Date
+}
 
 async function main(){
     let biblioteca1 = criarBiblioteca()
@@ -31,11 +35,13 @@ async function main(){
     adicionarLivroNaBiblioteca(biblioteca1, livro7)
     adicionarLivroNaBiblioteca(biblioteca1, livro8)
 
-    let livroEmprestado1 = emprestarLivro(biblioteca1, 'Ilíada e Odisséisa')
-    let livroEmprestado2 = emprestarLivro(biblioteca1, 'Lord of the Rings')
-    readicionandoLivro(biblioteca1, livroEmprestado1)
-    readicionandoLivro(biblioteca1, livroEmprestado2)
+    let livroEmprestado1= emprestarLivro(biblioteca1, 'Ilíada e Odisséisa', new Date())
+    let livroEmprestado2 = emprestarLivro(biblioteca1, 'Lord of the Rings', new Date("2024-05-01"))
+    let recibo1 = devolverLivro(biblioteca1, livroEmprestado1, new Date("2024-05-15"))
+    let recibo2 = devolverLivro(biblioteca1, livroEmprestado2, new Date("2024-05-15"))
+
     listagemDosLivros(biblioteca1)
+    console.log(recibo1, recibo2)
 }
 function criarLivro(nome: string, autor: string): Livro {
     return {
@@ -63,22 +69,33 @@ function listagemDosLivros(checagem: Biblioteca) {
         console.log(`Nome - ${el.nome};  Autor - ${el.autor}`)
     })
 }
-function emprestarLivro(lista: Biblioteca, nomeLivro: string): Emprestimo {
+function emprestarLivro(lista: Biblioteca, nomeLivro: string, dataEmprestimo: Date): Emprestimo {
     for (let i: number = 0; i < lista.livros.length; i++) {
         let el: Livro = lista.livros[i]
         if (nomeLivro === el.nome) {
             lista.livros.splice(i, 1)
-            let data = new Date()
-            data.setDate(data.getDate()+7)
+            let dataContrato = new Date(dataEmprestimo.getTime())
+            dataContrato.setDate(dataContrato.getDate()+7)
             return {
                 livro: el,
-                dataContrato: data,
+                dataContrato: dataContrato,
             }
         }
     }
     return undefined
 }
-function readicionandoLivro (emprestar: Biblioteca, livro: Livro){
-    emprestar.livros.push(livro)
+function devolverLivro (biblioteca: Biblioteca, emprestimo: Emprestimo, dataDevolucao: Date): Recibo {
+    biblioteca.livros.push(emprestimo.livro)
+    let diff: number = dataDevolucao.getTime() - emprestimo.dataContrato.getTime()
+    let diferencaDeDias: number = Math.ceil(diff / (1000 * 60 * 60 * 24))
+    let multa= 0
+    if (diferencaDeDias > 0) {
+        multa = diferencaDeDias * 2
+    }
+    return  {
+        multa: multa,
+        dataDevolucao: dataDevolucao
+    }
 }
+
 main()
