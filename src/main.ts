bcb4555aac801cd4321db50330f4da44a2bf2bd4
Pedro Henrique <vazpedro06@gmail.com>
2024-05-31 17:53:23 -0300

    adicionando testes. Jest

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ const route = Router()
 app.use(express.json())
 
 const biblioteca = new Biblioteca();
+const livros: Livro[] = [];
 
 route.get('/biblioteca', (req: Request, res: Response) => {
     res.json(biblioteca.livros)
@@ -29,14 +30,55 @@ route.get('/biblioteca/devolver-livro', (req: Request, res: Response) => {
     // res.json(emprestimo)
     res.json({ message: 'devolver livro pra biblioteca' })
 })
-route.get('/livro', (req: Request, res: Response) => {
-    res.json({ message: 'criar um livro' })
+
+route.post('/livros', (req: Request, res: Response) => {
+    const livro = new Livro(req.body['nome'], req.body['autor'])
+    livros.push(livro)
+    res.json(livros.length - 1)
 })
-route.get('/toUpperCase/:text', (req: Request, res: Response) => {
-    res.json({ text: req.params["text"].toUpperCase() })
+
+route.get('/livros', (req: Request, res: Response) => {
+    res.json(livros.filter(el => !!el))
+})
+
+route.get('/livros/:id', (req: Request, res: Response) => {
+    const livro = livros[+req.params.id];
+    if (!livro) {
+        res.status(404)
+        res.json("não existe")
+        return
+    }
+
+    res.json(livro)
+})
+
+route.patch('/livros/:id', (req: Request, res: Response) => {
+    const livro = livros[+req.params.id];
+    if (!livro) {
+        res.status(404)
+        res.json("não existe")
+        return
+    }
+
+    livro.nome = req.body['nome']
+    livro.autor = req.body['autor']
+
+    res.json(livro)
+})
+
+route.delete('/livros/:id', (req: Request, res: Response) => {
+    let livro = livros[+req.params.id];
+    if (!livro) {
+        res.status(404)
+        res.json("não existe")
+        return
+    }
+
+    livros[+req.params.id] = undefined
+
+    res.status(204)
+    res.json()
 })
 
 app.use(route)
-
-
-app.listen(3333, () => 'server running on port 3333')
+app.listen(8080, () => 'server running on port 8080')
